a4682d6983ba4d2f4e635ce183c4923cae0bdf33
avi21-lakra <avinashlakra0414@gmail.com>
2026-01-09 18:59:33 +0530

    url change

--- frontend/app.py
+++ frontend/app.py
@@ -10,7 +10,7 @@ if "chat_history" not in st.session_state:
 # =====================================================
 # CONFIG
 # =====================================================
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://ecom-analytics-wak7.onrender.com"
 
 st.set_page_config(
     page_title="E-Commerce Business Analytics",
